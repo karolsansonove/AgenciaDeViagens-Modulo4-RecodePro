4682c46af30aa343ef0170bacc4ad59712bc8336
Karoline Ribeiro <karolsansonove@gmail.com>
2022-02-04 16:41:01 -0300

    ajuste na localização do driver JDBC; select adicionado no UF do form createDestino.jsp

--- AgenciaDeViagensViaggio/src/main/java/factory/ConnectionFactory.java
+++ AgenciaDeViagensViaggio/src/main/java/factory/ConnectionFactory.java
@@ -7,10 +7,9 @@ import java.sql.SQLException;
 public class ConnectionFactory {
 	private static final String USERNAME = "root";
     private static final String PASSWORD = "root#123";
-    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/AgenciaViagem";
+    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/agenciaviagem";
 
     public static Connection createConnection() throws Exception {
-        // Class.forName("com.mysql.cj.jdbc.Driver");
         Class.forName("com.mysql.jdbc.Driver");
         Connection conn = null;
 
